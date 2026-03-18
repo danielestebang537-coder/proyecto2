33f3a7729c844ff5932ddf1623f692aebaaa2f9c
Andres H <candres90@gmail.com>
2026-03-17 21:43:26 -0500

    change url

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 export const env = {
     port: Number(process.env.PORT) || 3000,
-    mongoUri: process.env.MONGO_URI || 'mongodb+srv://etitcmongodb:qaz123*@cluster0.24yhi29.mongodb.net/',
+    mongoUri: process.env.MONGO_URI || '',
     mongoDbName: process.env.MONGO_DB_NAME || 'test'
 }
